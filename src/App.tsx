28b6073b17fece775a3efc1a3bdd170bbfc2e8b7
Vitaly Turovsky <vital2580@icloud.com>
2020-12-03 15:19:04 +0300

    ilya-work
    Co-authored-by: Илья Белов <bkmz.0000@mail.ru>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ComponentProps {
 }
+
+let App: React.FC<ComponentProps> = () => {
+    const canvasRef = useRef(null as null | HTMLCanvasElement);
+
+    useEffect(() => {
+        const canvasElem = canvasRef.current!;
+        const gl = canvasElem.getContext("webgl")!;
+        gl.clearColor(0, 0, 0, 1);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    }, []);
+
+    return <canvas ref={canvasRef}></canvas>;
+};
 
 export default App;
